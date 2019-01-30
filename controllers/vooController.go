ebfa50a82a32d32354d375ddfa14537f8ba02091
klaupacius <cloudklaupacius@gmail.com>
2019-01-30 14:20:57 -0200

    Adicionando configurações no Swagger

--- controllers/vooController.go
+++ controllers/vooController.go
@@ -18,10 +18,11 @@ type VooController struct{}
 
 // ShowAccount godoc
 // @Summary Show a account
-// @Description Retorna rodos os voos cadastrados
+// @Description Retorna todos os voos cadastrados
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} models.Voo
+// @Failure 404 {string} string "ok"
 // @Router /voo [get]
 func (VooController) BuscarVoos(w http.ResponseWriter, r *http.Request) {
 
@@ -31,7 +32,18 @@ func (VooController) BuscarVoos(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// ShowAccount godoc
+// @Summary Show a account
+// @Description Retorna o voo cadastrado com base no idVoo passado
+// @Accept  json
+// @Produce  json
+// @Param idVoo path int true "IdVoo"
+// @Success 200 {object} models.Voo
+// @Failure 404 {string} string "ok"
+// @Router /voo/{idVoo} [get]
 func (VooController) BuscarVooPorId(w http.ResponseWriter, r *http.Request) {
+	var voo models.Voo
+	
 	// pega parametros passados
 	id := chi.URLParam(r, "idVoo")
 
@@ -42,12 +54,22 @@ func (VooController) BuscarVooPorId(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// serviço que retorna o voo com base no idVoo passado
-	voo := vooService.CarregarVoo(idVoo)
+	voo = vooService.CarregarVoo(idVoo)
 
 	// adiciona como reposta o voo retornado
 	json.NewEncoder(w).Encode(voo)
 
 }
+
+// ShowAccount godoc
+// @Summary Show a account
+// @Description Metodo que cadastrar voos
+// @Accept  json
+// @Produce  json
+// @Param idVoo path int true "IdVoo"
+// @Success 200 
+// @Failure 404 {string} string "ok"
+// @Router /voo [post]
 func (VooController) CadastrarVoo(w http.ResponseWriter, r *http.Request) {
 	var vooDto dtos.VooDto
 
@@ -73,7 +95,17 @@ func (VooController) ExcluirVoo(w http.ResponseWriter, r *http.Request) {
 	utils.RespondwithJSON(w, http.StatusOK, map[string]string{"message": "Voo excluido"})
 }
 
+// ShowAccount godoc
+// @Summary Show a account
+// @Description Metodo para atualizar um voo
+// @Accept  json
+// @Produce  json
+
+// @Success 200 
+// @Failure 404 {string} string "ok"
+// @Router /voo [put]
 func (VooController) AtualizarVoo(w http.ResponseWriter, r *http.Request) {
+	
 	// voo que será adicionado
 	var voo models.Voo
 
