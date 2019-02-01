d131fd76737a7d0427ba496271cfa0303874b890
Claudiano <claudiano@github.com>
2019-01-31 22:40:32 -0300

    alterando conexão com o banco para deploy

--- repositories/conexao.go
+++ repositories/conexao.go
@@ -7,14 +7,12 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-const (
-	Drive            string = "postgres"
-	IpServidor       string = "localhost"
-	Port             int    = 5432
-	DbName           string = "flydb"
-	UserDatabase     string = "postgres"
-	PasswordDatabase string = "root@123"
-)
+const Drive string = "postgres"
+const IpServidor string = "    ec2-54-221-253-228.compute-1.amazonaws.com"
+const Port int = 5432
+const DbName string = "d38sqrk7l2kvjf"
+const UserDatabase string = "vujqrnurjsmsdq"
+const PasswordDatabase string = "    ed3a701d30a956440705558947568e4553b6f1919f33ac7ddf32a8490fa382b5"
 
 func CriarTabelas() {
 	db := connectar()
@@ -41,7 +39,11 @@ func CriarTabelas() {
 func connectar() *gorm.DB {
 
 	// Abrindo conexão com o banco
-	db, err := gorm.Open(Drive, "host=localhost port=5432 user=postgres dbname=flydb password=root@123 sslmode=disable")
+	//db, err := gorm.Open(Drive, "host=localhost port=5432 user=postgres dbname=flydb password=root@123 sslmode=disable")
+	db, err := gorm.Open(Drive, "host="+IpServidor+" port=5432 user="+
+		UserDatabase+" dbname="+DbName+" password="+PasswordDatabase+
+		" sslmode=disable")
+
 	if err != nil {
 		panic(err)
 	}
